521049c9067b3e5a2a7cc7045bab3557c2362298
Patrik <patrikdrean@gmail.com>
2020-01-29 18:55:52 -0700

    Remove Jest watch flag Add comments about Jest syntax

--- src/example.test.js
+++ src/example.test.js
@@ -1,5 +1,16 @@
 import { add } from './example'
 
+// describe():
+//// First param: name of the test suite
+//// Second param: a function with the actual tests inside
+
+// it() or test():
+//// First param: name of the test
+//// Second param: a function with the actual test logic and assertion statement inside
+
+// expect().toEqual()
+//// compares the calculated value to what is expected
+
 describe('add', () => {
   it('adds positive numbers together', () => {
     const result = add(3, 5)
